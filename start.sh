ead9a06ee5dffaadd8f3094acf6808b734a22d88
Daniel Chick <daniel.chick@hotmail.com>
2021-05-26 16:58:31 -0500

    add echo

--- start.sh
+++ start.sh
@@ -23,6 +23,7 @@ fi
 
 cd ${INPUT_DIRECTORY}
 
+echo "Repository ${REPOSITORY}"
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
 git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
